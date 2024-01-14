74b06e1b98f7d544a78e6aac5127527f0b7eb5ff
Johann Schopplich <mail@johannschopplich.com>
2024-01-14 23:10:19 +0100

    chore: update lockfile

--- scripts/ploi-deploy.sh
+++ scripts/ploi-deploy.sh
@@ -9,8 +9,6 @@ composer install --no-interaction --prefer-dist --optimize-autoloader --no-dev
 {RELOAD_PHP_FPM}
 
 # Build playground plugin
-if [ -f pnpm-lock.yaml ]; then
-  pnpm i && pnpm run build:playground
-fi
+pnpm i && pnpm run build:playground
 
 echo "🚀 Playground deployed!"
