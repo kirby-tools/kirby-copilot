01acb35352d9f8c559489875f348c87fd8f03b2a
Johann Schopplich <mail@johannschopplich.com>
2024-01-24 16:37:21 +0100

    fix: require Kirby 4

--- index.php
+++ index.php
@@ -1,6 +1,12 @@
 <?php
 
 use Kirby\Cms\App as Kirby;
+use Composer\Semver\Semver;
+
+// Validate Kirby version
+if (!Semver::satisfies(Kirby::version() ?? '0.0.0', '~4.0')) {
+    throw new Exception('Kirby Copilot requires Kirby 4');
+}
 
 Kirby::plugin('johannschopplich/copilot', [
     'api' => require __DIR__ . '/src/extensions/api.php',
