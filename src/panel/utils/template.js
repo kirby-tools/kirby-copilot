50b6974bff1c1fe833ce651e3144a106f235693f
Johann Schopplich <mail@johannschopplich.com>
2024-01-29 15:25:37 +0100

    fix: convert key match to lowercase

--- src/panel/utils/template.js
+++ src/panel/utils/template.js
@@ -2,7 +2,7 @@ export function template(str, vars, fallback) {
   return str.replace(
     /{([\w\d]+)}/g,
     (_, key) =>
-      vars[key] ||
+      vars[key.toLowerCase()] ||
       ((typeof fallback === "function" ? fallback(key) : fallback) ?? key),
   );
 }
