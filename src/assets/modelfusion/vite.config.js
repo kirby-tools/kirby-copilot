27dbc6f852281cc4e6d64718561d464c88a4a395
Johann Schopplich <mail@johannschopplich.com>
2024-03-19 20:16:25 +0100

    refactor: playground

--- src/assets/modelfusion/vite.config.js
+++ src/assets/modelfusion/vite.config.js
@@ -1,8 +1,6 @@
 import { resolve } from "node:path";
-import { fileURLToPath } from "node:url";
 import { defineConfig } from "vite";
-
-const rootDir = fileURLToPath(new URL("../../../", import.meta.url));
+import { rootDir } from "../constants";
 
 export default defineConfig({
   build: {
