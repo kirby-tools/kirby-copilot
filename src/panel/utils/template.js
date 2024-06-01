a60348e45f38a07db3f18f47e1399a3714c000be
Johann Schopplich <mail@johannschopplich.com>
2024-06-01 10:41:27 +0200

    fix: unescaped regex chars

--- src/panel/utils/template.js
+++ src/panel/utils/template.js
@@ -1,6 +1,6 @@
 export function renderTemplate(str, variables, fallback) {
   return str.replace(
-    /{([\w\d]+)}/g,
+    /\{(\w+)\}/g,
     (_, key) =>
       variables[key.toLowerCase()] ||
       ((typeof fallback === "function" ? fallback(key) : fallback) ?? key),
