9d1d7b2be22e1675b558593d68ccd9dfdae628f2
Johann Schopplich <mail@johannschopplich.com>
2024-12-16 18:01:20 +0100

    feat!: update default system prompt

--- src/panel/constants.js
+++ src/panel/constants.js
@@ -18,7 +18,7 @@ Instructions:
 - If pdf_document_pages are provided, additional context from PDF documents have been processed and made available to you. Consider the information from these documents as applicable.
 
 Output Format:
-- If response_format is set to "text", format your response in plain text. Do not include any special formatting.
-- If response_format is set to "HTML", format your response using HTML syntax. Do not include the <head> section or any other parts of a full HTML document structure, only the content of the <body> element.
-- If response_format is set to "markdown", format your response using Markdown syntax. Do not use backticks or any other wrapping characters around your Markdown.
+- If response_format is set to "text", format your response in plain text.
+- If response_format is set to "HTML", format your response using HTML syntax. Do not include any other parts of a full HTML document structure, except for the content of the <body> element. Structure your response using appropriate HTML tags. Use <h2> or <h3> tags for section headings.
+- If response_format is set to "markdown", format your response using Markdown syntax. Do not use backticks or any other wrapping characters around your response.
 `;
