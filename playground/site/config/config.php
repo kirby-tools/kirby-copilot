e4eccaa0b13df457c33d4facfd1f7f1c4128b348
Johann Schopplich <mail@johannschopplich.com>
2025-12-31 21:44:25 +0100

    chore: update playground config

--- playground/site/config/config.php
+++ playground/site/config/config.php
@@ -24,12 +24,15 @@ return [
         'provider' => 'google',
         'providers' => [
             'openai' => [
-                'model' => 'gpt-5-mini',
+                'model' => 'gpt-5.2',
                 'apiKey' => env('OPENAI_API_KEY', 'YOUR_API_KEY')
             ],
             'google' => [
                 'model' => 'gemini-3-flash-preview',
                 'apiKey' => env('GOOGLE_API_KEY', 'YOUR_API_KEY')
+            ],
+            'anthropic' => [
+                'apiKey' => env('ANTHROPIC_API_KEY', 'YOUR_API_KEY')
             ]
         ]
     ]
